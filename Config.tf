3f0d0629b08ad9a5f8b23e4eeb971f5931b94cdf
Mylo Cyrus <irahiggins740@gmail.com>
2022-07-07 16:54:57 -0700

    Update Config.tf

--- Config.tf
+++ Config.tf
@@ -1,1 +1,5 @@
-Create file debug all
+1. #  block-definition-block-content-delimited
+   "{" and "}", starting on the same line as the block header.
+2. # Create file debug all
+
+3. *Define block header
